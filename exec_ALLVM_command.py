a1611645c5f1f76b62ea074bb4306ede2cce5910
Ubuntu <student@lab000001.yshc10dcdz1e5kdl1u0kn5dzba.px.internal.cloudapp.net>
2025-08-21 10:08:02 +0000

    got basic functionality working

--- exec_ALLVM_command.py
+++ exec_ALLVM_command.py
@@ -1,7 +1,32 @@
 #!/bin/python3
 
+import os
 
 from backup_VM_configs import routing_table
 
+# todo: currently can't handle sudo level stuff, or anything involving a login prompt
+def main():
+    
+    comm = input("$>")
 
-print(routing_table)
+    for hostname in routing_table:
+        ip = routing_table[hostname]
+        
+        # todo: remove sshpass in favour of silent PKI handshakes. unfortunately
+        # a tool is needed to deposit the public keys in the first place, so you 
+        # can't make one without the other :)
+        print("Sending to: ", hostname, ".student441...")
+        res = os.popen(f"sshpass -p {os.environ['SUPER_SECRET_SSH_PASSWORD']} ssh student@{ip} {comm}")
+        if res is None:
+            print("[!] ERROR: command send failed.")
+        print(f"[ {hostname}.student441 ]=====================")
+        print(res.read())
+        print(f"==============================================")
+
+
+    print("[+] All command sends finished.")
+        
+
+
+if __name__ == "__main__":
+    main()
